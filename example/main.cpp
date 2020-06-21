8fb713e5cbdb0342c6fd720140db650849a5153a
Tom de Geus <tom@geus.me>
2020-06-21 17:28:47 +0200

    Fixing CI

--- example/main.cpp
+++ example/main.cpp
@@ -3,31 +3,11 @@
 // =================================================================================================
 
 static const char USAGE[] =
-R"(Run
-  Slowly decrease force from a certain initial force, until the system stops. Measure the force
-  and the local distance to yielding at the final configuration.
-
-  Input data:
-  * "/N": number of particles.
-  * "/dt": time step.
-  * "/eta": damping coefficient (uniform).
-  * "/m": mass (uniform).
-  * "/mu": elastic stiffness (uniform).
-  * "/k": mean-field coupling stiffness (uniform).
-  * "/fext": external force (constant).
-  * "/alpha": Gamma yield positions distribution - shape / alpha / k.
-  * "/beta": Gamma yield positions distribution - scale / beta / theta.
-  * "/tau": Noise decay time.
-  * "/noise": Noise amplitude.
-  * "/fext_0": Initial force.
-  * "/dot_fext": Force decrease rate.
-
-  Output data:
-  * "/f_c": Force at which system stops.
-  " "/x_sigma": Distance to yielding of each particle.
+R"(Foo
+  bar
 
 Usage:
-  Run [options] <data.hdf5>
+  Run [options]
 
 Options:
   -h, --help      Show help.
